77501008aff9e817ca925de1b144d7f39aad8628
aaron <harms280@gmail.com>
2016-01-12 15:49:41 -0800

    add bootstrap, change layout of signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
