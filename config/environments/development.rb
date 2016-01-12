d41b9924499d7ace2ca8dc0deb9a1c77602ba4df
Aaron Harms <aaronharms@Aarons-MacBook.local>
2016-01-11 22:52:03 -0800

    set up user model with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  #ADDED FOR DEVISE ****must replace localhost with actual host of application for production
+  config.action_mailer.default_url_options = { host: 'locahost', port: 3000}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
