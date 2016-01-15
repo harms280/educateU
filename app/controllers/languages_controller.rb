f019ff5c01419621ddc9f569b317c7f4e03e85dd
aaron <harms280@gmail.com>
2016-01-14 20:18:51 -0800

    add methods to language controller

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,2 +1,11 @@
 class LanguagesController < ApplicationController
+  def index
+    @languages = Language.all()
+  end
+
+  def show
+    @language = Language.find_by_id params[:id]
+    @courses = LanguageCourse.where(language_id: params[:id])
+    binding.pry
+  end
 end
