500cb3bab6f1bedb58a853890fc54b7765154fd1
Aaron Harms <harms280@gmail.com>
2016-01-17 14:05:32 -0800

    prevent user from writing many reviews for one course

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
+  before_action :prevent_multiple_reviews, only: [:new, :create]
 
   def new
     @course = Course.find(params[:course_id])
@@ -10,7 +11,6 @@ class ReviewsController < ApplicationController
     @course = Course.find_by_id params[:course_id]
     @review = @course.reviews.build(review_params)
     @review.user_id = current_user.id
-    binding.pry
     if @review.save 
       redirect_to @course, flash: {success: "Review Successfully Created"}
     else
@@ -18,9 +18,28 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
 
   def review_params
     params.require(:review).permit(:title,:rating,:review_body)
+  end
+
+  def prevent_multiple_reviews
+    @course = Course.find_by_id params[:course_id]
+    unless current_user.reviews.where(course_id: params[:course_id]).empty?
+      redirect_to @course, flash: {warning: "Already reviewed course"}
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+	def bootstrap_class_for flash_type
+	    { success: "alert-success", error: "alert-danger", alert: "alert-danger", notice: "alert-info", warning: "alert-warning" }[flash_type.to_sym] || flash_type.to_s
+	end
+
+	def bootstrap_icon_for flash_type
+	    { success: "ok-circle", error: "remove-circle", alert: "warning-sign", notice: "exclamation-sign", warning: 'exclamation-sign' }[flash_type.to_sym] || "question-sign"
+	end
+
+	def flash_messages(opts = {})
+		flash.each do |msg_type, message|
+		  concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in text-center") do 
+		          concat content_tag(:button, 'x'.html_safe , class: "close", data: { dismiss: 'alert' })
+		          concat message 
+		        end)
+		end
+		nil
+	end
 end
